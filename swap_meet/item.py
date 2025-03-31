a797b4f98251d516c9f1f93ded7be8b2dd2ad226
Malik Elmessiry <malik.messiry@gmail.com>
2025-04-01 01:08:09 +0200

    completed wave 2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,17 @@
+import uuid #imported this to generate id
+
 class Item:
-    pass
+    def __init__(self, id=None):
+        if id == None:
+            self.id = uuid.uuid4().int
+        else:
+            self.id = id
+
+
+    def get_category(self):
+        return type(self).__name__ #looked this up and found this built-in function
+        
+
+
+
+
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,5 @@
+from swap_meet.item import Item #imported item
+
 class Vendor:
     def __init__(self, inventory=None):
         if inventory == None:
@@ -14,3 +16,9 @@ class Vendor:
             self.inventory.remove(item)
             return item
         return False
+    
+    def get_by_id(self, item_id): #added this method
+        for item in self.inventory:
+            if item_id == item.id:
+                return item
+        return None
