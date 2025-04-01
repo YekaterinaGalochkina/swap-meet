e2e1516ec82292d75ef8303463e033dc0c27de86
Malik Elmessiry <malik.messiry@gmail.com>
2025-04-01 22:30:12 +0200

    wave 5 without inheritance

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,30 @@
+import uuid
+
 class Decor:
-    pass
+    def __init__(self, id=None, width=0, length=0, condition=0):
+        if id == None:
+            self.id = uuid.uuid4().int
+        else:
+            self.id = id
+        self.width = width
+        self.length = length
+        self.condition = condition
+    
+    def get_category(self):
+        return type(self).__name__
+    
+    def __str__(self):
+        return f"An object of type {self.get_category()} with id {self.id}. It takes up a {self.width} by {self.length} sized space."
+    
+    def condition_description(self):
+        description = {
+            0: "very bad condition",
+            1: "bad condition",
+            2: "fair condition",
+            3: "okay condition",
+            4: "almost new condition",
+            5: "new condition"
+        }
+        for key, value in description.items():
+            if self.condition == key:
+                return value
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,11 +1,12 @@
 import uuid #imported this to generate id
 
 class Item:
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0):
         if id == None:
             self.id = uuid.uuid4().int
         else:
             self.id = id
+        self.condition = condition
 
 
     def get_category(self):
@@ -15,5 +16,17 @@ class Item:
     def __str__(self): # overwrite default behavior of the built-in stringify
         return f"An object of type {self.get_category()} with id {self.id}."
     
+    def condition_description(self):
+        description = {
+            0: "very bad condition",
+            1: "bad condition",
+            2: "fair condition",
+            3: "okay condition",
+            4: "almost new condition",
+            5: "new condition"
+        }
+        for key, value in description.items():
+            if self.condition == key:
+                return value
 
 
