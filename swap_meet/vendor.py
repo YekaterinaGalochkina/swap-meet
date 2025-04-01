9c493c71eb3e2de86e50eef8625d555b73ff4ae2
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-03-31 20:52:38 -0700

    Implementing Wave 4

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -31,3 +31,16 @@ class Vendor:
         other_vendor.inventory.remove(their_item)
         self.inventory.append(their_item)
         return True
+    
+    # Wave 4
+    def swap_first_item(self, other_vendor):
+        if self.inventory == [] or other_vendor.inventory == []:
+            return False
+        first_item_vendor = self.inventory[0]
+        first_item_friends = other_vendor.inventory[0]
+        self.inventory.remove(first_item_vendor)
+        self.inventory.append(first_item_friends)
+        other_vendor.inventory.remove(first_item_friends)
+        other_vendor.inventory.append(first_item_vendor)
+        return True
+
